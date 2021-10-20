a38c0b989f8603127be57ed92c80c3a85271907f
Tshepiso <timarape@gmail.com>
2021-10-20 20:34:14 +0200

    Added Redis Cache Example

---
+++ DesinPatternsApplication/MementoDesignPattern/BasicMemento/FoodExample/FoodSupplier.cs
@@ -0,0 +1,57 @@
+﻿namespace MementoDesignPattern.BasicMemento.FoodExample
+{
+    /// <summary>
+    /// This is the originator  
+    /// </summary>
+    public class FoodSupplier
+    {
+        #region Fields
+        private string _name;
+        private string _phone;
+        private string _address;
+
+        #endregion
+
+
+        #region Properties
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+
+        public string Phone
+        {
+            get { return _phone; }
+            set { _phone = value; }
+        }
+
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        #endregion
+
+
+        #region Methods
+        public Mememto SaveMemento()
+        {
+            return new Mememto(_name, _phone, _address);
+        }
+
+        public void RestoreMemento(Mememto mememto)
+        {
+            Name = mememto.Name;
+            Phone = mememto.PhoneNumber;
+            Address = mememto.Address;
+        }
+        #endregion
+
+    }
+}
+
+
+
---
+++ DesinPatternsApplication/MementoDesignPattern/BasicMemento/FoodExample/SupplierCaretaker.cs
@@ -0,0 +1,14 @@
+﻿namespace MementoDesignPattern.BasicMemento.FoodExample
+{
+    public class SupplierCaretaker
+    {
+        private Mememto _memento;
+
+        public Mememto Memento
+        {
+            get { return _memento; }
+            set { _memento = value; }
+        }
+
+    }
+}
